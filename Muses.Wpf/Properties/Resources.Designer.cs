f0bbaa53f80e4c175d3889cb484a468bf53a78cc
Jan van den Baard <jcbaar@outlook.com>
2018-01-16 15:35:41 +0700

    - Added a first version of menu styles.

--- Muses.Wpf/Properties/Resources.Designer.cs
+++ Muses.Wpf/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Muses.Wpf.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -57,6 +57,33 @@ namespace Muses.Wpf.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Copy.
+        /// </summary>
+        public static string Menu_Copy {
+            get {
+                return ResourceManager.GetString("Menu_Copy", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Cut.
+        /// </summary>
+        public static string Menu_Cut {
+            get {
+                return ResourceManager.GetString("Menu_Cut", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Paste.
+        /// </summary>
+        public static string Menu_Paste {
+            get {
+                return ResourceManager.GetString("Menu_Paste", resourceCulture);
             }
         }
         
