53dc70a7e0a03ed740c43fd5c39adb7cda559e80
Jan van den Baard <jcbaar@outlook.com>
2018-02-11 11:24:34 +0100

    - More small tweaks for the TextBox

---
+++ Muses.Wpf/Converters/BooleanToVisibilityHiddenConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Muses.Wpf.Converters
+{
+    /// <summary>
+    /// Converter class which returns a settable <see cref="Visibility"/> value when
+    /// the value is false. By default <see cref="Visibility.Hidden"/> is returned for
+    /// a false value.
+    /// </summary>
+    public class BooleanToVisibilityHiddenConverter : IValueConverter
+    {
+        public Visibility FalseValue { get; set; } = Visibility.Hidden;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is Boolean)
+            {
+                return (bool)value == false ? FalseValue : Visibility.Visible;
+            }
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException("BooleanToVisibilityHiddenConverter can only be used OneWay.");
+        }
+    }
+}
